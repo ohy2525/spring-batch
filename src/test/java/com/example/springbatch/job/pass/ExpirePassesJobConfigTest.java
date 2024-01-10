6e388f61f35d4b9997ac21d4191bf2d550bf28e1
ohy2525 <dhgodbs2@naver.com>
2024-01-10 23:26:45 +0900

    Batch : 이용 기간에 따른 만료 테스트

---
+++ src/test/java/com/example/springbatch/job/pass/ExpirePassesJobConfigTest.java
@@ -0,0 +1,18 @@
+package com.example.springbatch.job.pass;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+@SpringBatchTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = {ExpirePassesJobConfig.class, TestBatchConfig.class})
+class ExpirePassesJobConfigTest {
+
+}
