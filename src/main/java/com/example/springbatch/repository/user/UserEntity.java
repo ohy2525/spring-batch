6e1f03f7f0bd79a1f3f0fbd3b28135370e908c09
ohy2525 <dhgodbs2@naver.com>
2024-01-20 23:48:45 +0900

    UserEntity 칼럼 추가

--- src/main/java/com/example/springbatch/repository/user/UserEntity.java
+++ src/main/java/com/example/springbatch/repository/user/UserEntity.java
@@ -5,6 +5,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.Map;
+
 
 @Getter
 @Setter
@@ -22,5 +27,16 @@ public class UserEntity extends BaseEntity {
     private UserStatus status;
 
     private String phone;
-    private String meta;
+
+    @Column(columnDefinition = "json")
+    @JdbcTypeCode(SqlTypes.JSON)
+    private Map<String, Object> meta;
+
+    public String getUuid() {
+        String uuid = null;
+        if (meta.containsKey("uuid")) {
+            uuid = String.valueOf(meta.get("uuid"));
+        }
+        return uuid;
+    }
 }
